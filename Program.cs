41e2b9eb6ef0d396736fe9cb6df5caecc293c0e9
Valeev <eleronsmarya@gmail.com>
2022-12-27 13:48:28 +0300

    KR2.sozdal_meto_obrabotki

--- Program.cs
+++ Program.cs
@@ -10,3 +10,21 @@ t= new string[N];
 for(int i=0;i<N;i++)
 t[i]=Console.ReadLine();
 string[] m= new string[N];
+
+void ArrayO(string[]t,string[]m)
+{
+   int k=0;
+   for(int i=0;i<N;i++)
+   {
+    foreach(char c in t[i])
+    {
+       k++;
+    }
+      if(k<=3)
+       m[i]= t[i];
+    
+    k=0;
+}
+}
+
+ArrayO(t,m);
